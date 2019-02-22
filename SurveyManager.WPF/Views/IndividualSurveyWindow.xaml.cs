4f90dfa985f086b29017ddbc6539e6ec686ec05e
Teng Wei Song <hi@tengweisong.com>
2019-02-22 17:19:30 +0800

    Created Individual Survey window

---
+++ SurveyManager.WPF/Views/IndividualSurveyWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SurveyManager.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for IndividualSurveyWindow.xaml
+    /// </summary>
+    public partial class IndividualSurveyWindow : Window
+    {
+        public IndividualSurveyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateSurveyDataTemplate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateReportDataTemplate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImportSurveyData(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImportReportData(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImportReportTemplate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateReport(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
