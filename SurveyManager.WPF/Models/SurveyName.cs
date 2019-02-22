cbd7c6c7abfbf4a50a21ab6e747f60783a0c8a29
Teng Wei Song <hi@tengweisong.com>
2019-02-22 17:46:25 +0800

    Added constant for survey names

---
+++ SurveyManager.WPF/Models/SurveyName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyManager.WPF.Models
+{
+    public static class SurveyName
+    {
+        public static string StudentSurveyOnLecturer = "CU Student Survey on Lecturer";
+        public static string UnitAndLecturerSurvey = "CC Unit and Lecturer Survey";
+    }
+}
