778fbea2c27e6bc35e5de82877db4a3335d7f496
Teng Wei Song <hi@tengweisong.com>
2019-02-22 17:46:44 +0800

    Added view model for IndividualSurveyWindow

---
+++ SurveyManager.WPF/ViewModels/IndividualSurveyWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyManager.WPF.ViewModels
+{
+    public class IndividualSurveyWindowViewModel : BaseViewModel
+    {
+        public IndividualSurveyWindowViewModel(string surveyName)
+        {
+
+        }
+
+        public void GenerateSurveyDataTemplate()
+        {
+
+        }
+
+        public void GenerateReportDataTemplate()
+        {
+
+        }
+    }
+}
--- SurveyManager.WPF/Views/IndividualSurveyWindow.xaml.cs
+++ SurveyManager.WPF/Views/IndividualSurveyWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SurveyManager.WPF.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,8 @@ namespace SurveyManager.WPF.Views
     /// </summary>
     public partial class IndividualSurveyWindow : Window
     {
+        public IndividualSurveyWindowViewModel ViewModel { get { return DataContext as IndividualSurveyWindowViewModel; } set { DataContext = value; } }
+
         public IndividualSurveyWindow()
         {
             InitializeComponent();
