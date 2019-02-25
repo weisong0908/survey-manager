4f1624150a42544d708773514e0b5f5ba8e79de1
Teng Wei Song <hi@tengweisong.com>
2019-02-25 14:09:50 +0800

    Created SurveyReport model

---
+++ SurveyManager.WPF/Models/SurveyReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyManager.WPF.Models
+{
+    public class SurveyReport
+    {
+        public int Id { get; set; }
+        public string UnitCode { get; set; }
+        public string UnitName { get; set; }
+        public string Lecturer { get; set; }
+        public string StudyTerm { get; set; }
+        public int ClassSize { get; set; }
+        public int Response { get; set; }
+        public IList<string> Alerts { get; set; }
+
+        public SurveyReport()
+        {
+            Alerts = new List<string>();
+        }
+    }
+}
